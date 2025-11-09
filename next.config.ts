7f8d9c988c5d11e18e9de9a13a03637ae1ac6ace
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-11-09 19:05:44 +0900

    chore: make it work, setup before code

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Only treat files with these specific names as pages/routes
-  pageExtensions: [
-    "page.tsx",
-    "page.ts",
-    "page.jsx",
-    "page.js",
-    "layout.tsx",
-    "layout.ts",
-    "layout.jsx",
-    "layout.js",
-    "route.ts",
-    "route.js",
-  ],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
